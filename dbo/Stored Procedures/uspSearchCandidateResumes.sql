dff92e97b5848ac9db97c613cf2a8c18197228b6
Tiago Palhota <mail@tiagopalhota.net>
2020-12-10 11:19:07 +0000

    REMOVED FREE TEXT SEARCH

--- dbo/Stored Procedures/uspSearchCandidateResumes.sql
+++ dbo/Stored Procedures/uspSearchCandidateResumes.sql
@@ -21,36 +21,37 @@ BEGIN
       END
       
 
-            --FREETEXTTABLE case as inflectional and Thesaurus were required
-      IF @useThesaurus = 1 AND @useInflectional = 1  
-        BEGIN
-                  SELECT FT_TBL.[JobCandidateID], KEY_TBL.[RANK] FROM [HumanResources].[JobCandidate] AS FT_TBL 
-                        INNER JOIN FREETEXTTABLE([HumanResources].[JobCandidate],*, @searchString,LANGUAGE @language) AS KEY_TBL
-                   ON  FT_TBL.[JobCandidateID] =KEY_TBL.[KEY]
-            END
+      --      --FREETEXTTABLE case as inflectional and Thesaurus were required
+      --IF @useThesaurus = 1 AND @useInflectional = 1  
+      --  BEGIN
+      --            SELECT FT_TBL.[JobCandidateID], KEY_TBL.[RANK] FROM [HumanResources].[JobCandidate] AS FT_TBL 
+      --                  INNER JOIN FREETEXTTABLE([HumanResources].[JobCandidate],*, @searchString,LANGUAGE @language) AS KEY_TBL
+      --             ON  FT_TBL.[JobCandidateID] =KEY_TBL.[KEY]
+      --      END
 
-      ELSE IF @useThesaurus = 1
-            BEGIN
-                  SELECT @string ='FORMSOF(THESAURUS,"'+@searchString +'"'+')'      
-                  SELECT FT_TBL.[JobCandidateID], KEY_TBL.[RANK] FROM [HumanResources].[JobCandidate] AS FT_TBL 
-                        INNER JOIN CONTAINSTABLE([HumanResources].[JobCandidate],*, @string,LANGUAGE @language) AS KEY_TBL
-                   ON  FT_TBL.[JobCandidateID] =KEY_TBL.[KEY]
-        END
+      --ELSE IF @useThesaurus = 1
+      --      BEGIN
+      --            SELECT @string ='FORMSOF(THESAURUS,"'+@searchString +'"'+')'      
+      --            SELECT FT_TBL.[JobCandidateID], KEY_TBL.[RANK] FROM [HumanResources].[JobCandidate] AS FT_TBL 
+      --                  INNER JOIN CONTAINSTABLE([HumanResources].[JobCandidate],*, @string,LANGUAGE @language) AS KEY_TBL
+      --             ON  FT_TBL.[JobCandidateID] =KEY_TBL.[KEY]
+      --  END
 
-      ELSE IF @useInflectional = 1
-            BEGIN
-                  SELECT @string ='FORMSOF(INFLECTIONAL,"'+@searchString +'"'+')'
-                  SELECT FT_TBL.[JobCandidateID], KEY_TBL.[RANK] FROM [HumanResources].[JobCandidate] AS FT_TBL 
-                        INNER JOIN CONTAINSTABLE([HumanResources].[JobCandidate],*, @string,LANGUAGE @language) AS KEY_TBL
-                   ON  FT_TBL.[JobCandidateID] =KEY_TBL.[KEY]
-        END
+      --ELSE IF @useInflectional = 1
+      --      BEGIN
+      --            SELECT @string ='FORMSOF(INFLECTIONAL,"'+@searchString +'"'+')'
+      --            SELECT FT_TBL.[JobCandidateID], KEY_TBL.[RANK] FROM [HumanResources].[JobCandidate] AS FT_TBL 
+      --                  INNER JOIN CONTAINSTABLE([HumanResources].[JobCandidate],*, @string,LANGUAGE @language) AS KEY_TBL
+      --             ON  FT_TBL.[JobCandidateID] =KEY_TBL.[KEY]
+      --  END
   
-      ELSE --base case, plain CONTAINSTABLE
-            BEGIN
-                  SELECT @string='"'+@searchString +'"'
-                  SELECT FT_TBL.[JobCandidateID],KEY_TBL.[RANK] FROM [HumanResources].[JobCandidate] AS FT_TBL 
-                        INNER JOIN CONTAINSTABLE([HumanResources].[JobCandidate],*,@string,LANGUAGE @language) AS KEY_TBL
-                   ON  FT_TBL.[JobCandidateID] =KEY_TBL.[KEY]
-            END
+      --ELSE --base case, plain CONTAINSTABLE
+      --      BEGIN
+      --            SELECT @string='"'+@searchString +'"'
+      --            SELECT FT_TBL.[JobCandidateID],KEY_TBL.[RANK] FROM [HumanResources].[JobCandidate] AS FT_TBL 
+      --                  INNER JOIN CONTAINSTABLE([HumanResources].[JobCandidate],*,@string,LANGUAGE @language) AS KEY_TBL
+      --             ON  FT_TBL.[JobCandidateID] =KEY_TBL.[KEY]
+      --      END
 
+	  select 'NO FREETEXT SEARCH INSTALLED'
 END;
